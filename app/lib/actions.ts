9182e875d82993a8a85bae29af455edcb7c0d2a1
Patrik <patriklevak@gmail.com>
2024-02-29 18:01:59 +0100

    Add new components for handling not found and error scenarios

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -71,9 +71,9 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
   } catch (errors) {
-    message: 'Database Error: Failed to Delete Invoice.';
+    return { message: 'Database Error: Failed to Delete Invoice.' };
   }
-  revalidatePath('/dashboard/invoices');
 }
