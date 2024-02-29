0992a7c6d1d9a3552ed47cd0363a908cab401403
Patrik <patriklevak@gmail.com>
2024-02-29 11:58:57 +0100

    chapter 12

--- scripts/seed.js
+++ scripts/seed.js
@@ -12,7 +12,7 @@ async function seedUsers(client) {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
     // Create the "users" table if it doesn't exist
     const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS users (
+      CREATE TABLE IF NOT EXISTS  users(
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email TEXT NOT NULL UNIQUE,
